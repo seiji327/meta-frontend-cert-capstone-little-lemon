25358876bd6e1446e6cf76fb124641d9a946e6fc
seiji327 <seiji.sagawa.ca@gmail.com>
2023-04-11 12:56:37 -0700

    basic responsiveness

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,28 +3,57 @@ import "./nav.css";
 
 const Nav = () => {
   return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#menu">Menu</a>
-        </li>
-        <li>
-          <a href="#reservations">Reservations</a>
-        </li>
-        <li>
-          <a href="#order-online">Order Online</a>
-        </li>
-        <li>
-          <a href="#login">Login</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#menu">Menu</a>
+          </li>
+          <li>
+            <a href="#reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="#order-online">Order Online</a>
+          </li>
+          <li>
+            <a href="#login">Login</a>
+          </li>
+        </ul>
+      </nav>
+      <img
+        src="/hamburger-icon.svg"
+        alt="hamburger menu icon"
+        className="hamburger"
+      />
+      <nav className="mobile-nav">
+        <ul className="mobile-nav-list">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#menu">Menu</a>
+          </li>
+          <li>
+            <a href="#reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="#order-online">Order Online</a>
+          </li>
+          <li>
+            <a href="#login">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
